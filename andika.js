28409dbe1c98749e332a0ffde3a377de97e04ea0
IPutuAndikaPutraPratama <sixanputra@gmail.com>
2024-08-19 14:16:06 +0800

    Add files via upload

---
+++ andika.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
